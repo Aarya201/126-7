720c1af4f82a4b6ec7ff65f2d88c819218df7c8b
Aarya201 <76944790+Aarya201@users.noreply.github.com>
2022-01-18 15:23:57 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -34,7 +34,7 @@ function gotPoses(results)
 
         RightWristx = results[0].pose.rightWrist.x;
         RightWristy = results[0].pose.rightWrist.y;
-        console.log("rightWristX = " + rightWristX +" rightWristY = "+ rightWristY);
+        console.log("RightWristx = " + RightWristx +" RightWristy = "+ RightWristy);
 
         LeftWristx = results[0].pose.leftWrist.x;
         LeftWristy = results[0].pose.leftWrist.y;
@@ -48,29 +48,29 @@ function draw(){
     fill("#FF0000");
 	stroke("#FF0000");
 
-    if(scoreRightWrist > 0.2)
+    if(scoreRightWrist > 0.002)
     {
-        circle(rightWristX,rightWristY,20);
+        circle(RightWristx,RightWristy,20);
 
-        if(rightWristY > 0 && rightWristY <= 100) {
+        if(RightWristy > 0 && RightWristy <= 100) {
             document.getElementById("speed").innerHTML = "Speed = 0.5x";
             song.rate(0.5);
-        }else if(rightWristY > 100 && rightWristY <= 200) {
+        }else if(RightWristy > 100 && RightWristy <= 200) {
             document.getElementById("speed").innerHTML = "Speed = 1x";
             song.rate(1);
-        }else if(rightWristY > 200 && rightWristY <= 300) {
+        }else if(RightWristy > 200 && RightWristy <= 300) {
             document.getElementById("speed").innerHTML = "Speed = 1.5x";
             song.rate(1.5);
-        }else if(rightWristY > 300 && rightWristY <= 400) {
+        }else if(RightWristy > 300 && RightWristy <= 400) {
             document.getElementById("speed").innerHTML = "Speed = 2x";
             song.rate(2);
-        }else  if(rightWristY > 400) {
+        }else  if(RightWristy > 400) {
             document.getElementById("speed").innerHTML = "Speed = 2.5x";
             song.rate(2.5);
         }
 
     }
-        if(scoreLeftWrist > 0.2)
+        if(scoreLeftWrist > 0.002)
         {
             circle(LeftWristx,LeftWristy,20);
             InNumberleftWristy = Number(LeftWristy);
